045958ace9a0a73d3b20a5af9af6b7a0080989d4
coronadoibanezluisfelipe@gmail.com <coronadoibanezluisfelipe@gmail.com>
2026-03-06 08:51:44 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;   
+const mysql = require('mysql2');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+// Configuración de la conexión a la base de datos
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'empleados_crud'
+});
+
+app.post("/create", (req, res) => {
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const pais = req.body.pais;
+    const cargo = req.body.cargo;
+    const anios = req.body.anios;
+    db.query("INSERT INTO empleados (nombre, edad, pais, cargo, anios) VALUES (?, ?, ?, ?, ?)",
+    [nombre, edad, pais, cargo, anios], (err, result) => {
+        if (err) { 
+        console.log(err); 
+    } else {
+            res.send("Empleado registrado");
+        }           
+    });
+});
+
+app.get("/empleados", (req, res) => {
+    db.query("SELECT * FROM empleados", (err, result) => {
+        if (err) {  
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+}); 
+
+app.put("/update/:id", (req, res) => {
+    const id = req.params.id;
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const pais = req.body.pais;
+    const cargo = req.body.cargo;
+    const anios = req.body.anios;
+    db.query("UPDATE empleados SET nombre=?, edad=?, pais=?, cargo=?, anios=? WHERE id=?", [nombre, edad, pais, cargo, anios, id], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send("Empleado actualizado");
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
